b56d3a7aa90a51da293c870e5e4e11ec1bbbf587
iDiogoRenan <diogo_renan2006@hotmail.com>
2026-05-11 22:05:44 -0300

    chore: brand window title

--- dublagem-master-tauri/src/shared/app/metadata.ts
+++ dublagem-master-tauri/src/shared/app/metadata.ts
@@ -3,4 +3,4 @@ const APP_VERSION_MINOR = 0;
 
 export const APP_NAME = "NSG Gaming Dub";
 export const APP_VERSION = [APP_VERSION_MAJOR, APP_VERSION_MINOR].join(".");
-export const APP_DISPLAY_NAME = `${APP_NAME} ${APP_VERSION}`;
+export const APP_DISPLAY_NAME = `${APP_NAME} ${APP_VERSION} - iDiogoRenan`;
